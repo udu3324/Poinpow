affc849daa091ce045a3d4ce4da429e36be1ef44
udu3324 <47045986+udu3324@users.noreply.github.com>
2023-04-30 01:04:08 -0500

    add another regex pattern

--- src/main/java/com/udu3324/poinpow/utils/BlockMinehutAds.java
+++ src/main/java/com/udu3324/poinpow/utils/BlockMinehutAds.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 
 public class BlockMinehutAds {
     public static String name = "block_minehut_ads";
-    public static String description = "Blocks ads made by minehut that sometimes shows up in free sub-servers.";
+    public static String description = "Blocks ads made by minehut that show up before joining servers & ads in free sub-servers.";
     public static AtomicBoolean toggled = new AtomicBoolean(true);
 
     public static Boolean check(String chat, CallbackInfo ci) {
@@ -18,13 +18,15 @@ public class BlockMinehutAds {
         // return if not on minehut
         if (!Poinpow.onMinehut) return false;
 
-        Pattern pattern = Pattern.compile("^(\\n\\n|/n/n)\\[Minehut].*(\\n\\n|/n/n)$");
+        Pattern pattern1 = Pattern.compile("^(\\n\\n|/n/n)\\[Minehut].*(\\n\\n|/n/n)$");
+        Pattern pattern2 = Pattern.compile("^Welcome to Minehut! Start your Free Minecraft server now!");
 
-        if (pattern.matcher(chat).find()) {
+        if (pattern1.matcher(chat).find() || pattern2.matcher(chat).find()) {
             Poinpow.log.info("Blocked: " + chat);
             ci.cancel();
-        }
 
-        return pattern.matcher(chat).find();
+            return true;
+        }
+        return false;
     }
 }
