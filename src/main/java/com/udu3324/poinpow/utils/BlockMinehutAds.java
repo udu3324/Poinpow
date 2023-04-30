d61e84d178f1105d19e5064f204f12fe6dd6267b
udu3324 <47045986+udu3324@users.noreply.github.com>
2023-04-30 01:05:50 -0500

    add another regex pattern

--- src/main/java/com/udu3324/poinpow/utils/BlockMinehutAds.java
+++ src/main/java/com/udu3324/poinpow/utils/BlockMinehutAds.java
@@ -18,13 +18,15 @@ public class BlockMinehutAds {
         // return if not on minehut
         if (!Poinpow.onMinehut) return false;
 
-        Pattern pattern = Pattern.compile("^(\\n\\n|/n/n)\\[Minehut].*(\\n\\n|/n/n)$");
+        Pattern pattern1 = Pattern.compile("^(\\n\\n|/n/n)\\[Minehut].*(\\n\\n|/n/n)$");
+        Pattern pattern2 = Pattern.compile("^Welcome to Minehut! Start your Free Minecraft server now!");
 
-        if (pattern.matcher(chat).find()) {
+        if (pattern1.matcher(chat).find() || pattern2.matcher(chat).find()) {
             Poinpow.log.info("Blocked: " + chat);
             ci.cancel();
-        }
 
-        return pattern.matcher(chat).find();
+            return true;
+        }
+        return false;
     }
 }
