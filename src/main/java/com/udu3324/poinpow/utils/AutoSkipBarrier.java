8850707d1e05f2d2c29204922d2e9b04977e7e99
Alex Yuen <buggyal.dev@gmail.com>
2023-08-03 11:45:30 -0500

    more cleanup

--- src/main/java/com/udu3324/poinpow/utils/AutoSkipBarrier.java
+++ src/main/java/com/udu3324/poinpow/utils/AutoSkipBarrier.java
@@ -11,12 +11,9 @@ public class AutoSkipBarrier {
     public static String name = "auto_skip_barrier";
     public static String description = "Auto-skips the ads when joining free sub-servers/minehut.";
     public static AtomicBoolean toggled = new AtomicBoolean(true);
-    public static void rename() {
-        // return if toggled off (no need for bool)
-        if (!toggled.get()) return;
 
-        // return if not on minehut
-        if (!Poinpow.onMinehut) return;
+    public static void rename() {
+        if (!toggled.get() || !Poinpow.onMinehut) return;
 
         new Thread(() -> {
             try {
@@ -26,19 +23,13 @@ public class AutoSkipBarrier {
 
                     MinecraftClient client = MinecraftClient.getInstance();
 
-                    // return if player hasn't loaded in it
-                    if (client.player == null) return;
-
-                    if (client.interactionManager == null) return;
+                    if (client.player == null || client.interactionManager == null) return;
 
                     ItemStack items;
                     for (int itemIterator = 0; itemIterator < 9; itemIterator++) {
                         items = client.player.getInventory().getStack(itemIterator);
                         if (items.getName().getString().equals("Right click to continue")) {
-                            // set item slot to wherever a item with this name is
                             client.player.getInventory().selectedSlot = itemIterator;
-                            
-                            //send right click
                             client.interactionManager.interactItem(client.player, client.player.getActiveHand());
                         } else {
                             tries++;
