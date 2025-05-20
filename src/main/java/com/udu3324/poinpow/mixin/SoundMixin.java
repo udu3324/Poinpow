61c31650bf5638395ed8a2531f718fc0685aa517
udu3324 <builder7177@gmail.com>
2025-05-19 19:04:49 -0500

    add sound mixin

---
+++ src/main/java/com/udu3324/poinpow/mixin/SoundMixin.java
@@ -0,0 +1,16 @@
+package com.udu3324.poinpow.mixin;
+
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public class SoundMixin {
+    @Inject(method = "onPlaySound", at = @At("HEAD"), cancellable = true)
+    private void onPlaySound(PlaySoundS2CPacket packet, CallbackInfo ci) {
+        //todo remove resource pack ad sound in lobby
+    }
+}
