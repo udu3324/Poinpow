b71c79e2e81f402191997b438d829380bdc9e56f
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-06-06 20:06:42 -0500

    Update readme + extend string check to lower false positives

--- src/main/java/com/udu3324/poinpow/utils/BlockBoopAnnounce.java
+++ src/main/java/com/udu3324/poinpow/utils/BlockBoopAnnounce.java
@@ -9,6 +9,7 @@ public class BlockBoopAnnounce {
     public static String name = "block_boop_announce";
     public static String description = "Blocks the boop arena announcements in the lobby.";
     public static AtomicBoolean toggled = new AtomicBoolean(true);
+
     public static Boolean check(String chat, CallbackInfo ci) {
         // return false if toggled off
         if (!toggled.get()) return false;
@@ -17,7 +18,7 @@ public class BlockBoopAnnounce {
         if (!Poinpow.onMinehut) return false;
 
         //return if it's not a boop message
-        if (!chat.toLowerCase().contains("try to boop them out")) return false;
+        if (!chat.contains(" Boop Arena! Try to boop them out and stay inside to stake your claim on the leaderboard")) return false;
 
         Poinpow.log.info("Blocked: {}", chat.replace("\n", ""));
         ci.cancel();
